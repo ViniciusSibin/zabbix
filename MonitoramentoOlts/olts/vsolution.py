c3a9a598796205019f764eb097b121d5504a656a
Vinicius Sibin <vinisibim@gmail.com>
2023-02-13 16:17:00 -0300

    Criado variáveis com os OIDs para coleta

--- MonitoramentoOlts/olts/vsolution.py
+++ MonitoramentoOlts/olts/vsolution.py
@@ -18,8 +18,9 @@ def vsolution():
         ip = olts[2]
         community = olts[5]
 
-        #Atualiza o nome da OLT no banco
+        #Atualiza o nome das OLTs no banco de dados
         sysName = ['1.3.6.1.2.1.1.5']
+        """
         for oid in sysName:
             result = get_oid_index(ip, community, oid)
             if result:
@@ -28,9 +29,31 @@ def vsolution():
                     bd.execute(updateNome)
                     conn.commit()
                     print(olts[0], '-->', olts[1], '-->', olts[2] , '-->', result)
+                # elif result[0][1] == '':
+                #     print("OLTs Sem nome")
+                #     print(olts[0], '-->', olts[1], '-->', olts[2] , '-->', result)
+        """
+        #Coleta as informações das PONs
+        oid_pon_index = ['.3.6.1.4.1.37950.1.1.5.10.1.2.1.1.1']
+        oid_pon_nome = ['.3.6.1.4.1.37950.1.1.5.10.1.2.1.1.2']
+        oid_pon_xorrente = ['.3.6.1.4.1.37950.1.1.5.10.13.1.1.4']
+        oid_pon_tx_Power = ['.3.6.1.4.1.37950.1.1.5.10.13.1.1.5'] 
+        oid_pon_status = ['.3.6.1.4.1.37950.1.1.5.10.1.2.1.1.3']
+        oid_pon_tensao = ['.3.6.1.4.1.37950.1.1.5.10.13.1.1.3']
+        oid_pon_temperatura = ['.3.6.1.4.1.37950.1.1.5.10.13.1.1.2']
 
-        #Atualiza as PONs
-        ponPortDownSpeed = ['1.3.6.1.4.1.37950.1.1.5.10.1.2.2.1.24']
+        #Coleta as informações das PONs
+        oid_onu_onu_index = ['1.3.6.1.4.1.37950.1.1.6.1.1.2.1.2']
+        oid_onu_pon_Index = ['1.3.6.1.4.1.37950.1.1.6.1.1.2.1.1']
+        oid_onu_posição = ['']
+        oid_onu_numero_serial = ['1.3.6.1.4.1.37950.1.1.6.1.1.2.1.5.{PONINDEX}']
+        oid_onu_temperatura = ['1.3.6.1.4.1.37950.1.1.6.1.1.3.1.3.{PONINDEX}']
+        oid_onu_tensao = ['1.3.6.1.4.1.37950.1.1.6.1.1.3.1.4.{PONINDEX}']
+        oid_onu_corrente = ['1.3.6.1.4.1.37950.1.1.6.1.1.3.1.5.{PONINDEX}']
+        oid_onu_rx_Power = ['1.3.6.1.4.1.37950.1.1.6.1.1.3.1.7.{PONINDEX}']
+        oid_onu_tx_Power = ['1.3.6.1.4.1.37950.1.1.6.1.1.3.1.6.{PONINDEX}']
+        oid_onu_status = ['1.3.6.1.4.1.37950.1.1.6.1.1.1.1.4.{PONINDEX}']
+
 
     # Fechando a conexão
     conn.close()
