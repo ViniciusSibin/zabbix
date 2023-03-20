1ccea0003d32f486f8f19afb3bcc5df471ac3259
Vinicius Sibin <vinisibim@gmail.com>
2023-03-20 12:35:18 -0300

    criado a contagem de onus autorizadas

--- MonitoramentoOlts/olts/vsolution.py
+++ MonitoramentoOlts/olts/vsolution.py
@@ -18,14 +18,14 @@ resultados = bd.fetchall()
 for olts in resultados:
     # Configurações de conexão SNMP
     ip = olts[2]
-    community = olts[5]
+    community = olts[6]
     # ip = '192.168.21.1'
     # community = 'mgp'
 
     #######################################
     ############### P O N s ###############
     #######################################
-    
+
     # OIDs a serem consultados
     pon_index_oid = ["1.3.6.1.4.1.37950.1.1.5.10.1.2.1.1.1"]
     pon_nome_oid = "1.3.6.1.4.1.37950.1.1.5.10.1.2.1.1.2"
@@ -43,6 +43,7 @@ for olts in resultados:
             continue
         else:
             for i in pon_index:
+                
                 #Salvando o index nos OIDs
                 pon_nome = pon_nome_oid + "." + str(i)
                 pon_corrente = pon_corrente_oid + "." + str(i)
@@ -53,37 +54,119 @@ for olts in resultados:
                 
                 #Consultando os valores dos OIDs
                 pon_nome_val = consult_single_oid(community, ip, pon_nome)
-                if '0x504f' in pon_nome_val or not pon_nome_val:
+                if not pon_nome_val or '0x504f' in pon_nome_val or pon_nome_val == '':
                     # tratamento do valor encontrado
                     pon_nome_val = f'PON0/{i}'
                 pon_corrente_val = consult_single_oid(community, ip, pon_corrente)
-                print(pon_corrente_val)
                 pon_tx_power_val = consult_single_oid(community, ip, pon_tx_power)
                 pon_status_val = consult_single_oid(community, ip, pon_status)
                 pon_tensao_val = consult_single_oid(community, ip, pon_tensao)
                 pon_temperatura_val = consult_single_oid(community, ip, pon_temperatura)
 
-                pon_corrente_val = re.sub('[^0-9\.]', '', pon_corrente_val)
-                pon_tx_power_val = re.sub('[^0-9\.]', '', pon_tx_power_val)
-                pon_tensao_val = re.sub('[^0-9\.]', '', pon_tensao_val)
-                pon_temperatura_val = re.sub('[^0-9\.]', '', pon_temperatura_val)
+                pon_corrente_val = re.sub('[^0-9\.-]', '', str(pon_corrente_val))
+                pon_tx_power_val = re.sub('[^0-9\.-]', '', str(pon_tx_power_val))
+                pon_tensao_val = re.sub('[^0-9\.-]', '', str(pon_tensao_val))
+                pon_temperatura_val = re.sub('[^0-9\.-]', '', str(pon_temperatura_val))
 
                 # Cria a string de comando SQL para inserir no banco de dados
-                query = f"INSERT INTO pon (olt_id, pon_index, slot_porta, autorizados, amperagem, tensao, tx_power, status, temperatura) VALUES ({olts[0]}, {i}, '{pon_nome_val}', {pon_corrente_val}, {pon_tensao_val}, {pon_tx_power_val}, {pon_status_val}, {pon_temperatura_val})"
-                print(query)
+                queryPON = f"INSERT INTO pon (olt_id, pon_index, slot_porta, amperagem, tensao, tx_power, status, temperatura) VALUES ({olts[0]}, {i}, '{pon_nome_val}', '{pon_corrente_val}', '{pon_tensao_val}', '{pon_tx_power_val}', '{pon_status_val}', '{pon_temperatura_val}')"
+                print(queryPON)
                 # Executa a query SQL
-                #bd.execute(query)
+                bd.execute(queryPON)
 
                 # Salva as alterações no banco de dados
-                #conn.commit()
+                conn.commit()
 
                 
+                #######################################
+                ############### O N U s ###############
+                #######################################
+                #Verificando se é GPON
+                if olts[4] == 'GPON':
+                    # OIDs a serem consultados
+                    
+                    onu_index_oid = ["1.3.6.1.4.1.37950.1.1.6.1.1.2.1.2" + "." + str(i)]
+                    onu_serial_oid = "1.3.6.1.4.1.37950.1.1.6.1.1.2.1.5" + "." + str(i)
+                    onu_corrente_oid = "1.3.6.1.4.1.37950.1.1.6.1.1.3.1.5" + "." + str(i)
+                    onu_tx_power_oid = "1.3.6.1.4.1.37950.1.1.6.1.1.3.1.6" + "." + str(i)
+                    onu_rx_power_oid = "1.3.6.1.4.1.37950.1.1.6.1.1.3.1.7" + "." + str(i)
+                    onu_status_oid = "1.3.6.1.4.1.37950.1.1.6.1.1.1.1.4" + "." + str(i)
+                    onu_tensao_oid = "1.3.6.1.4.1.37950.1.1.6.1.1.3.1.4" + "." + str(i)
+                    onu_temperatura_oid = "1.3.6.1.4.1.37950.1.1.6.1.1.3.1.3" + "." + str(i)
 
+                    #contagem de ONUs autorizadas
+                    autorizadas = 0
 
-    #######################################
-    ############### O N U s ###############
-    #######################################
+                    # Consulta o OID de índice PON
+                    for oidONU in onu_index_oid:
+                        onu_index = get_oid_index(ip, community, oidONU)
+                        if not onu_index:
+                            print('ONU Não encontrada')
+                            continue
+                        else:
+                            for onuIndex in onu_index:
+                                onu_serial = onu_serial_oid + "." + str(onuIndex)
+                                onu_corrente = onu_corrente_oid + "." + str(onuIndex)
+                                onu_tx_power = onu_tx_power_oid + "." + str(onuIndex)
+                                onu_rx_power = onu_rx_power_oid + "." + str(onuIndex)
+                                onu_status = onu_status_oid + "." + str(onuIndex)
+                                onu_tensao = onu_tensao_oid + "." + str(onuIndex)
+                                onu_temperatura = onu_temperatura_oid + "." + str(onuIndex)
+                                
+                                #Consultando os valores dos OIDs
+                                onu_serial_val = consult_single_oid(community, ip, onu_serial)
+                                if not onu_serial_val or '0x504f' in onu_serial_val or onu_serial_val == '':
+                                    # tratamento do valor encontrado
+                                    onu_serial_val = f'PON0/{i}'
+                                onu_corrente_val = consult_single_oid(community, ip, onu_corrente)
+                                onu_tx_power_val = consult_single_oid(community, ip, onu_tx_power)
+                                onu_rx_power_val = consult_single_oid(community, ip, onu_rx_power)
+                                onu_status_val = consult_single_oid(community, ip, onu_status)
+                                onu_tensao_val = consult_single_oid(community, ip, onu_tensao)
+                                onu_temperatura_val = consult_single_oid(community, ip, onu_temperatura)
 
+                                #deixando somente valores númericos
+                                onu_corrente_val = re.sub('[^0-9\.-]', '', str(onu_corrente_val))
+                                onu_tx_power_val = re.sub('[^0-9\.-]', '', str(onu_tx_power_val))
+                                onu_rx_power_val = re.sub('[^0-9\.-]', '', str(onu_rx_power_val))
+                                onu_tensao_val = re.sub('[^0-9\.-]', '', str(onu_tensao_val))
+                                onu_temperatura_val = re.sub('[^0-9\.-]', '', str(onu_temperatura_val))
 
-    # Fecha a conexão com o banco de dados
-    #conn.close()
+                                #coletando pon_id do banco de dados
+                                ponQuery = f"SELECT id, slot_porta FROM pon where olt_id='{olts[0]}' AND slot_porta='{pon_nome_val}'"
+                                bd.execute(ponQuery)
+
+                                # Obtendo os resultados da consulta
+                                pon_resultado = bd.fetchall()
+                                if len(pon_resultado) > 0:
+                                    ponID = pon_resultado[0][0]
+                                    
+                                    #Criando a posicao
+                                    posicao = str(pon_resultado[0][1]) + "/" + str(onuIndex)
+                                else:
+                                    print("Nenhum resultado encontrado")
+                                
+                                # Cria a string de comando SQL para inserir no banco de dados
+                                autorizadas = autorizadas + 1
+                                print('Autorizadas:', autorizadas)
+                                queryONU = f"INSERT INTO onu (pon_id, onu_index, posicao, status, sn, temperatura, voltagem, amperagem, rx_power, tx_power) VALUES ('{ponID}', '{onuIndex}', '{posicao}', '{onu_status_val}', '{onu_serial_val}', '{onu_temperatura_val}', '{onu_tensao_val}', '{onu_corrente_val}', '{onu_rx_power_val}', '{onu_tx_power_val}')"
+                                print(queryONU)
+                                # Executa a query SQL
+                                bd.execute(queryONU)
+
+                                # Salva as alterações no banco de dados
+                                conn.commit()
+
+                    queryAutorizados = f"UPDATE pon SET autorizados='{autorizadas}' WHERE id='{ponID}';"
+                    # Executa a query SQL
+                    bd.execute(queryAutorizados)
+
+                    # Salva as alterações no banco de dados
+                    conn.commit()
+                #Verificando se é EPON                
+                elif olts[4] == 'EPON':
+                    print('OLT EPON')
+                else:
+                    print('OLT SEM PROTOCOLO')
+# Fecha a conexão com o banco de dados
+conn.close()
