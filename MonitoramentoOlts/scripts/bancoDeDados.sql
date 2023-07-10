fd7aeeb2365742ccaf97d4cca439fd9274586a5d
Vinicius Sibin <vinisibim@gmail.com>
2023-07-10 13:14:29 -0300

    Criado um novo banco de dados do zero para os novos scripts

--- MonitoramentoOlts/scripts/bancoDeDados.sql
+++ MonitoramentoOlts/scripts/bancoDeDados.sql
@@ -1,6 +1,8 @@
-CREATE DATABASE IF NOT EXISTS olt
+CREATE DATABASE IF NOT EXISTS monitoramento_OLTs
 	DEFAULT CHARACTER SET utf8
 	DEFAULT COLLATE utf8_general_ci;
+
+USE monitoramento_OLTs;
 
 CREATE TABLE IF NOT EXISTS olt(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -43,16 +45,94 @@ INSERT INTO olt (nome, ip, fabricante, versaoSNMP, comunidade, porta) VALUES('OL
 INSERT INTO olt (nome, ip, fabricante, versaoSNMP, comunidade, porta) VALUES('OLT-MGP-AURORA2', '10.254.2.2', 'HUAWEI', '2c', 'mgp', 23);
 INSERT INTO olt (nome, ip, fabricante, versaoSNMP, comunidade, porta) VALUES('OLT AGUA BOA', '10.254.1.169', 'VSOLUTION', '2c', 'mgp', 23);
 INSERT INTO olt (nome, ip, fabricante, versaoSNMP, comunidade, porta) VALUES('OLT_EPON-2_IGUA', '192.168.14.3', 'VSOLUTION', '2c', 'mgp', 23);
-INSERT INTO olt (nome, ip, fabricante, versaoSNMP, comunidade, porta) VALUES('VSOL-EPON-VILA', '192.168.14.5', 'VSOLUTION', '2c', 'mgp', 23);
 INSERT INTO olt (nome, ip, fabricante, versaoSNMP, comunidade, porta) VALUES('OLT - MGP', '10.254.1.132', 'FIBERHOME', '2c', 'mgp', 23);
 
 COMMIT;
+
+CREATE TABLE IF NOT EXISTS cidades(
+  id int(11) NOT NULL AUTO_INCREMENT,
+  nome varchar(50) NOT NULL,
+  estado varchar(50) NOT NULL,
+  pais varchar(50) NOT NULL,
+  status int(11) DEFAULT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+
+INSERT INTO cidades (nome, estado, pais, status) VALUES('Agua Boa', 'Paraná', 'Brasil', 1);
+INSERT INTO cidades (nome, estado, pais, status) VALUES('Angulo', 'Paraná', 'Brasil', 1);
+INSERT INTO cidades (nome, estado, pais, status) VALUES('Astorga', 'Paraná', 'Brasil', 1);
+INSERT INTO cidades (nome, estado, pais, status) VALUES('Doutor Camargo', 'Paraná', 'Brasil', 1);
+INSERT INTO cidades (nome, estado, pais, status) VALUES('Iguaracu', 'Paraná', 'Brasil', 1);
+INSERT INTO cidades (nome, estado, pais, status) VALUES('Iguatemi', 'Paraná', 'Brasil', 1);
+INSERT INTO cidades (nome, estado, pais, status) VALUES('Ivatuba', 'Paraná', 'Brasil', 1);
+INSERT INTO cidades (nome, estado, pais, status) VALUES('Maringa', 'Paraná', 'Brasil', 1);
+INSERT INTO cidades (nome, estado, pais, status) VALUES('Munhoz de Melo', 'Paraná', 'Brasil', 1);
+INSERT INTO cidades (nome, estado, pais, status) VALUES('Paicandu', 'Paraná', 'Brasil', 1);
+INSERT INTO cidades (nome, estado, pais, status) VALUES('Sarandi', 'Paraná', 'Brasil', 1);
+INSERT INTO cidades (nome, estado, pais, status) VALUES('Tupinamba', 'Paraná', 'Brasil', 1);
+INSERT INTO cidades (nome, estado, pais, status) VALUES('Mandaguacu', 'Paraná', 'Brasil', 1);
+INSERT INTO cidades (nome, estado, pais, status) VALUES('Dist. Floriano', 'Paraná', 'Brasil', 1);
+INSERT INTO cidades (nome, estado, pais, status) VALUES('Dist. São Domingos', 'Paraná', 'Brasil', 1);
+
+COMMIT;
+
+CREATE TABLE olt_cidade (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  id_olt int(11) NOT NULL,
+  id_cidade int(11) NOT NULL,
+  PRIMARY KEY (id),
+  KEY id_olt (id_olt),
+  KEY id_cidade (id_cidade),
+  CONSTRAINT olt_cidade_ibfk_1 FOREIGN KEY (id_olt) REFERENCES olt (id),
+  CONSTRAINT olt_cidade_ibfk_3 FOREIGN KEY (id_cidade) REFERENCES cidades (id)
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8;
+
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(1, 9);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(1, 3);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(1, 5);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(2, 11);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(3, 11);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(4, 10);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(5, 10);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(5, 15);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(6, 8);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(7, 6);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(7, 15);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(8, 13);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(9, 11);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(10, 11);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(11, 4);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(12, 8);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(13, 2);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(14, 8);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(15, 8);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(16, 14);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(17, 8);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(18, 8);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(19, 8);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(20, 8);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(21, 7);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(22, 8);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(23, 5);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(24, 8);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(25, 8);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(26, 8);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(27, 8);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(28, 8);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(29, 11);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(30, 1);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(31, 5);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(32, 8);
+INSERT INTO olt_cidade (id_olt, id_cidade) VALUES(32, 11);
+
+
 
 CREATE TABLE IF NOT EXISTS pon(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	olt_id INT NOT NULL,
 	pon_index INT,
 	slot_porta VARCHAR(9) NOT NULL,
+	descricao VARCHAR(30),
 	autorizados INT,
 	amperagem FLOAT,
 	tx_power FLOAT,
--- MonitoramentoOlts/scripts/conexao.py
+++ MonitoramentoOlts/scripts/conexao.py
@@ -1,4 +1,4 @@
 import mysql.connector
 
 # Conectando ao banco de dados
-conn = mysql.connector.connect(host='172.31.255.51', user='vinicius', passwd='A132546b', db='olt')
+conn = mysql.connector.connect(host='172.31.255.51', user='vinicius', passwd='A132546b', db='monitoramento_OLTs')
